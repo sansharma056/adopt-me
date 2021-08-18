6410f5316137f2d301feecc71ab272d8c3c1537e
Sankalp Sharma <sansharma056@gmail.com>
2021-08-18 13:14:36 +0530

    added class component ErrorBoundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
@@ -28,6 +29,7 @@ class Details extends Component {
 
     const { animal, breed, city, state, description, name, images } =
       this.state;
+
     return (
       <div className="details">
         <Carousel images={images} />
@@ -40,4 +42,12 @@ class Details extends Component {
   }
 }
 
-export default withRouter(Details);
+const DetailsWithRounter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRounter />
+    </ErrorBoundary>
+  );
+}
